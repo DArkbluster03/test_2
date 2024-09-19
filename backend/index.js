a3e06d5d1b27bfdd7077be3791f4312d02220a74
DArkbluster03 <darkbluster03@gmail.com>
2024-09-19 18:32:24 +0600

    qwerty

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
@@ -12,20 +12,32 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors({ 
 
+// CORS configuration
+const allowedOrigins = ['https://test-2-nfkh.vercel.app', 'https://front-sage.vercel.app']; // Add all allowed origins
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
 }));
 
+// Routes setup
 const authRoutes = require('./src/routes/auth.user');
 const blogRoutes = require('./src/routes/blog.route');
 const commentRoutes = require('./src/routes/comment.route');
 
-// Routes setup
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 app.use('/api/comments', commentRoutes);
 
-
+// MongoDB connection and main function
 async function main() {
   await mongoose.connect(process.env.MONGODB_URL);
   app.get('/', (req, res) => {
@@ -33,11 +45,9 @@ async function main() {
   });
 }
 
-
-
-main().then(() => console.log('Mongodb connected successfully!')).catch(err => console.log(err));
-
-
+main()
+  .then(() => console.log('Mongodb connected successfully!'))
+  .catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
